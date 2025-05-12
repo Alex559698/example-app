ed46122eab0a675afa1410ca7ccd44920c96f528
Tu Nombre <ctoapanta763@gmail.com>
2025-05-12 12:21:21 -0500

    Primer commit del proyecto Laravel

---
+++ database/migrations/2025_05_12_151944_create_productos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre'); // Columna para el nombre del producto
+            $table->decimal('precio', 8, 2); // Columna para el precio del producto
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+};
